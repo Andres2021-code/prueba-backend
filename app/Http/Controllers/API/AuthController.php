8bc72e534874d068f32dbb4cda79d82aaebbdb47
andres <tkpipe15@gmail.com>
2021-03-29 15:54:24 -0500

    implementacion de login de acceso

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -54,32 +54,18 @@ class AuthController extends ApiController
 
   }
 
-  public function getTipoUsuario(){
-    
-      $data = [];
-
-      $tipo_usuario = DB::table("tipo_usuario")
-          ->select("tipo_usuario.id", "tipo_usuario.nombre")
-          ->get();
-
-      $data['tipo_usuario'] = $tipo_usuario;
-
-      return $this->sendResponse($data, "tipo_usuarios recuperadas correctamente");
-  }
 
   public function login(Request $request)
     {
         
         $loginData = $request->validate([
-            'email' => 'email|required',
-            'password' => 'required',
-            'usuario_cedula' => 'required',
-            'tipo_id' => 'required'
+          'email' => 'required|email',
+          'password' => 'required',
+          'confirm_password' => '',
         ]);
 
-        $user =  DB::select('select * from users where email = :email and usuario_cedula = :usuario_cedula and
-         tipo_id = :tipo_id',
-         ['email' => $request->email, 'usuario_cedula' => $request->usuario_cedula, 'tipo_id' => $request->tipo_id]);
+        $user =  DB::select('select * from users where email = :email',
+         ['email' => $request->email]);
         
          $data['usuario'] = $user;
         
